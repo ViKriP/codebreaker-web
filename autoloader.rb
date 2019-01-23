8ffabc7770adbee089bf1f3fd5e79708c3f4251c
ViKriP <viktor_biz@ukr.net>
2019-01-23 20:36:06 +0200

    included menu

--- autoloader.rb
+++ autoloader.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'codebreaker'
-require 'haml'
+require 'erb'
+require 'i18n'
 require './lib/racker'
 require './lib/i18n_config'
---
+++ lib/racker.rb
@@ -0,0 +1,49 @@
+class Racker
+  attr_reader :request, :game
+
+  def self.call(env)
+    new(env).response.finish
+  end
+
+  def initialize(env)
+    @request = Rack::Request.new(env)
+  end
+
+  def response
+    case @request.path
+    when '/', '/win', '/lose' then check_game_existance
+    when '/start' then start
+    when '/game' then game
+    when '/rules', '/statistics' then static_pages
+    when '/hint' then hint
+    else Rack::Response.new(render('404'), 404)
+    end
+  end
+
+  private
+
+  def check_game_existance
+    return show_page('menu') unless current_game
+  end
+
+  def current_game
+    @request.session[:game]
+  end
+
+  def session_param_nil?(param)
+    @request.session[param].nil?
+  end
+
+  def t(phrase, *args)
+    I18n.t(phrase, *args)
+  end
+
+  def show_page(template)
+    Rack::Response.new(render(template))
+  end
+
+  def render(template)
+    path = File.expand_path("../views/#{template}.html.erb", __FILE__)
+    ERB.new(File.read(path)).result(binding)
+  end
+end
